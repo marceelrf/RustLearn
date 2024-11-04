0545360ec24f87f0585f9e0754a896e956096997
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2024-11-04 14:55:24 -0300

    adasd

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/marceelrf/Projs/RustLearn/branches/target/debug/branches: /home/marceelrf/Projs/RustLearn/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-faaf2033e74c0a69.d
@@ -0,0 +1,5 @@
+/home/marceelrf/Projs/RustLearn/branches/target/debug/deps/branches-faaf2033e74c0a69: src/main.rs
+
+/home/marceelrf/Projs/RustLearn/branches/target/debug/deps/branches-faaf2033e74c0a69.d: src/main.rs
+
+src/main.rs:
