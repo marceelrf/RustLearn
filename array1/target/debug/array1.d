fb0c21a46bf54cdaecc87467e97ca7fc24d34dfa
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2024-11-04 13:30:10 -0300

    array1

---
+++ array1/target/debug/array1.d
@@ -0,0 +1,1 @@
+/home/marceelrf/Projs/RustLearn/array1/target/debug/array1: /home/marceelrf/Projs/RustLearn/array1/src/main.rs
---
+++ array1/target/debug/deps/array1-4f6d3a7c725ff958.d
@@ -0,0 +1,5 @@
+/home/marceelrf/Projs/RustLearn/array1/target/debug/deps/array1-4f6d3a7c725ff958: src/main.rs
+
+/home/marceelrf/Projs/RustLearn/array1/target/debug/deps/array1-4f6d3a7c725ff958.d: src/main.rs
+
+src/main.rs:
