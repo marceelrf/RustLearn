e2913e342ad01fdb42cbbdc54ea266e81a9ec53b
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2024-11-04 13:35:41 -0300

    Functions

---
+++ functions/target/debug/deps/functions-5955fc710e00f209.d
@@ -0,0 +1,5 @@
+/home/marceelrf/Projs/RustLearn/functions/target/debug/deps/functions-5955fc710e00f209: src/main.rs
+
+/home/marceelrf/Projs/RustLearn/functions/target/debug/deps/functions-5955fc710e00f209.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/marceelrf/Projs/RustLearn/functions/target/debug/functions: /home/marceelrf/Projs/RustLearn/functions/src/main.rs
