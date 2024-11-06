167eef3dbf60d980fafce622d23fd81306e4897b
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2024-11-06 15:16:15 -0300

    loops

---
+++ loops/target/debug/deps/loops-2ce153b663589422.d
@@ -0,0 +1,5 @@
+/home/marceelrf/Projs/RustLearn/loops/target/debug/deps/loops-2ce153b663589422: src/main.rs
+
+/home/marceelrf/Projs/RustLearn/loops/target/debug/deps/loops-2ce153b663589422.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/home/marceelrf/Projs/RustLearn/loops/target/debug/loops: /home/marceelrf/Projs/RustLearn/loops/src/main.rs
