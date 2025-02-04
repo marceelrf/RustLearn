a46ab90a2623c93ee0b4320eb477f6dce335afa1
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2025-02-04 11:08:46 -0300

    New rust

---
+++ struct_ex/target/debug/deps/struct_ex-02d01cf41fba61ca.d
@@ -0,0 +1,5 @@
+/home/marceelrf/Projs/RustLearn/RustLearn/struct_ex/target/debug/deps/libstruct_ex-02d01cf41fba61ca.rmeta: src/main.rs
+
+/home/marceelrf/Projs/RustLearn/RustLearn/struct_ex/target/debug/deps/struct_ex-02d01cf41fba61ca.d: src/main.rs
+
+src/main.rs:
---
+++ struct_ex/target/debug/deps/struct_ex-2dc2a313c87d9f9b.d
@@ -0,0 +1,5 @@
+/home/marceelrf/Projs/RustLearn/RustLearn/struct_ex/target/debug/deps/libstruct_ex-2dc2a313c87d9f9b.rmeta: src/main.rs
+
+/home/marceelrf/Projs/RustLearn/RustLearn/struct_ex/target/debug/deps/struct_ex-2dc2a313c87d9f9b.d: src/main.rs
+
+src/main.rs:
