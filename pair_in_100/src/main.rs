1251d6d50f3207e21e49bce70ee4b96fd956cf89
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2025-02-15 19:59:48 -0300

    violet

--- pair_in_100/src/main.rs
+++ pair_in_100/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     for numero in 1..100 {
         if numero % 2 == 0 {
 
-            println!("{}", numero.to_string().bold().yellow().underline());
+            println!("{}", numero.to_string().bold().truecolor(128, 0, 255).underline());
 
         }
     }
