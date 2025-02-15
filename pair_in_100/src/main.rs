e9b9fd88e78041c14a16151e67d3bcf4259c6baf
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2025-02-15 20:11:12 -0300

    sys sleep

--- pair_in_100/src/main.rs
+++ pair_in_100/src/main.rs
@@ -1,10 +1,13 @@
 use colored::Colorize;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     for numero in 1..100 {
         if numero % 2 == 0 {
 
             println!("{}", numero.to_string().bold().truecolor(128, 0, 255).underline());
+            thread::sleep(Duration::from_millis(200));
 
         }
     }
